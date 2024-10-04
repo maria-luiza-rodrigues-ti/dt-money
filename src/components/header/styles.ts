e69bf9929cee115b70ea7a3ad442d7036e24f6dd
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-10-04 15:48:17 -0300

    style: adding mobile style to header

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -2,7 +2,17 @@ import { styled } from "styled-components";
 
 export const HeaderContainer = styled.header`
   background-color: ${(props) => props.theme['gray-900']};
-  padding: 2.5rem 0 7.5rem; 
+  padding: 2.5rem 0 7.5rem;
+
+  @media (max-width: 768px) {
+    padding: 1.5rem 1rem 6.625rem;
+  }
+
+  img {
+    @media(max-width: 768px) {
+      max-width: 117px;
+    }
+  }
 `
 
 export const HeaderContent = styled.div`
@@ -22,9 +32,17 @@ export const NewTransactionButton = styled.button`
   background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
   font-weight: bold;
+  line-height: 1.6;
   padding: 0 1.25rem;
   border-radius: 6px;
   cursor: pointer;
+
+  @media (max-width: 768px) {
+    height: 38px;
+    padding: 0 1rem;
+
+    font-size: .875rem;
+  }
 
   &:hover {
     background: ${(props) => props.theme['green-700']};
