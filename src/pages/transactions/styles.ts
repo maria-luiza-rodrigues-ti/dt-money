1f6652ef3ae33f6c7df905fa2828fa84c92827f2
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-10-04 15:14:47 -0300

    style: adding mobile style to transactions section

--- src/pages/transactions/styles.ts
+++ src/pages/transactions/styles.ts
@@ -5,6 +5,10 @@ export const TransactionsContainer = styled.main`
   max-width: 1120px;
   margin: 4rem auto 0;
   padding: 0 1.5rem;
+
+  @media (max-width: 768px) {
+    margin: 1.5rem auto 0;
+  }
 `
 
 export const TransactionsTable = styled.table`
@@ -13,18 +17,76 @@ export const TransactionsTable = styled.table`
   border-spacing: 0 0.5rem;
   margin-top: 1.5rem;
 
+  tr {
+
+    @media (max-width: 768px) {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto auto auto;
+
+      margin-bottom: .75rem;
+      padding: 1,5rem;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+
   td {
     padding: 1.25rem 2rem;
     background: ${(props) => props.theme['gray-700']};
 
+    line-height: 1.6;
+
     &:first-child {
       border-top-left-radius: 6px;
       border-bottom-left-radius: 6px;
+
+      @media (max-width: 768px) {
+        grid-column: 1 / 3;
+        grid-row: 1 / 2;
+
+        width: 100%;
+
+        border-top-right-radius: 6px;
+        padding: 1.5rem 1.5rem 0.25rem;
+      }
+    }
+
+    &:nth-child(2) {
+
+      @media (max-width: 768px) {
+          grid-column: 1 / 3;
+          grid-row: 2 / 3;
+          padding: 0 1.5rem;
+
+          font-size: 1.25rem;
+          font-weight: 700;
+          line-height: 1.6;
+      }
+    }
+
+    &:nth-child(3) {
+
+      @media (max-width: 768px) {
+        border-bottom-left-radius: 6px;
+        padding: .75rem 1.5rem 1.5rem;
+
+        color: ${(props) => props.theme['gray-500']};
+      }
     }
 
     &:last-child {
       border-top-right-radius: 6px;
-      border-bottom-right-radius: 6px;
+      border-bottom-right-radius: 6px; 
+
+      @media (max-width: 768px) {
+        padding: .75rem 1.5rem 1.5rem;
+        text-align: right;
+
+        color: ${(props) => props.theme['gray-500']};
+      }
     }
   }
 `
