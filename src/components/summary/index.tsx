e5e258df2c6992c8adf21a112678fe8f534cdc62
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-10-04 11:57:11 -0300

    refact: changing summary to slider and refactoring desktop and mobile styles

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -1,3 +1,4 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { priceFormatter } from "../../utils/formatter";
 import { useSummary } from "../../hooks/useSummary";
 
@@ -7,6 +8,7 @@ import {
   CurrencyDollar,
 } from "@phosphor-icons/react";
 
+import "@splidejs/react-splide/css/core";
 import { SummaryCard, SummaryContainer } from "./styles";
 
 export function Summary() {
@@ -14,30 +16,52 @@ export function Summary() {
 
   return (
     <SummaryContainer>
-      <SummaryCard>
-        <header>
-          <span>Entradas</span>
-          <ArrowCircleUp size={32} color="#00b37e" />
-        </header>
+      <Splide
+        options={{
+          perPage: 3,
+          arrows: false,
+          pagination: false,
+          gap: "2rem",
+          breakpoints: {
+            640: {
+              perPage: 1,
+              perMove: 1,
+              padding: { left: "0", right: "20%" },
+            },
+          },
+        }}
+      >
+        <SplideSlide>
+          <SummaryCard>
+            <header>
+              <span>Entradas</span>
+              <ArrowCircleUp size={32} color="#00b37e" />
+            </header>
 
-        <strong>{priceFormatter.format(summary.income)}</strong>
-      </SummaryCard>
-      <SummaryCard>
-        <header>
-          <span>Saídas</span>
-          <ArrowCircleDown size={32} color="#f75a68" />
-        </header>
+            <strong>{priceFormatter.format(summary.income)}</strong>
+          </SummaryCard>
+        </SplideSlide>
+        <SplideSlide>
+          <SummaryCard>
+            <header>
+              <span>Saídas</span>
+              <ArrowCircleDown size={32} color="#f75a68" />
+            </header>
 
-        <strong>{priceFormatter.format(summary.outcome)}</strong>
-      </SummaryCard>
-      <SummaryCard variant="green">
-        <header>
-          <span>Entradas</span>
-          <CurrencyDollar size={32} color="#fff" />
-        </header>
+            <strong>{priceFormatter.format(summary.outcome)}</strong>
+          </SummaryCard>
+        </SplideSlide>
+        <SplideSlide>
+          <SummaryCard variant="green">
+            <header>
+              <span>Entradas</span>
+              <CurrencyDollar size={32} color="#fff" />
+            </header>
 
-        <strong>{priceFormatter.format(summary.total)}</strong>
-      </SummaryCard>
+            <strong>{priceFormatter.format(summary.total)}</strong>
+          </SummaryCard>
+        </SplideSlide>
+      </Splide>
     </SummaryContainer>
   );
 }
--- src/components/summary/styles.ts
+++ src/components/summary/styles.ts
@@ -4,13 +4,11 @@ export const SummaryContainer = styled.section`
   width: 100%;
   max-width: 1120px;
   margin: 0 auto;
-  padding: 0 1.5rem;
-
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 2rem;
-
   margin-top: -5rem;
+
+  @media (max-width: 768px) {
+    padding-left: 1.5rem;
+  }
 `
 
 interface SummaryCardProps {
